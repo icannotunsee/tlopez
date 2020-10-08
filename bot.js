5c1c2dcef03bf7de923c25697ecd46a493b57fac
icannotunsee <66531908+icannotunsee@users.noreply.github.com>
2020-10-08 10:42:07 -0500

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,20 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready', () => {
+    console.log(`Logged in as ${client.user.tag}!`);
+    client.user.setActivity('unsee content', { type: 'STREAMING' });
+});
+
+
+  client.on('message', message => {
+    const args = message.content.split(" ").slice(1);
+    if(message.content.startsWith('t!say') && message.guild.member(message.author).hasPermission("MANAGE_ROLES")) {
+        message.delete()
+        var saytext = args.join(" ");
+        message.channel.send(saytext);
+    }
+  });
+
+
+  client.login(process.env.BOT_TOKEN);
