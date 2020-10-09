9f7492d2c0f2a793b6362c36bdb18091178c0278
icannotunsee <66531908+icannotunsee@users.noreply.github.com>
2020-10-08 20:06:00 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,5 +1,43 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
+const { CommandoClient } = require('discord.js-commando');
+const { Structures } = require('discord.js');
+const path = require('path');
+const { prefix, token } = require('./config.json');
+
+Structures.extend('Guild', Guild => {
+  class MusicGuild extends Guild {
+    constructor(client, data) {
+      super(client, data);
+      this.musicData = {
+        queue: [],
+        isPlaying: false,
+        volume: 1,
+        songDispatcher: null
+      };
+    }
+  }
+  return MusicGuild;
+});
+const client = new CommandoClient({
+  commandPrefix: prefix,
+  owner: '566279767544823808',
+  unknownCommandResponse: false
+});
+
+client.registry
+  .registerDefaultTypes()
+  .registerGroups([
+    ['music', 'Music Command Group']
+  ])
+  .registerDefaultGroups()
+  .registerDefaultCommands()
+  .registerCommandsIn(path.join(__dirname, 'commands'));
+
+client.once('ready', () => {
+  console.log('Ready!');
+});
+
 
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
